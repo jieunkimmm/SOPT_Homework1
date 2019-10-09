2232232b0a0f0d3191a8ea2c61fbdcbb2aa1dc0e
jieunkimmm <jieun7332@hanmail.net>
2019-10-10 00:41:28 +0900

    안드 1차세미나 과제

--- app/src/main/java/com/example/sopt_homework1/MainActivity.kt
+++ app/src/main/java/com/example/sopt_homework1/MainActivity.kt
@@ -1,40 +1,72 @@
 package com.example.sopt_homework1
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import kotlinx.android.synthetic.main.signup_layout.*
+import android.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
+    var signup_id: EditText? = null
+    var signup_pw: EditText? = null
+    var sign_up: Button? = null
+    var login: Button? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.signup_layout)
+        setContentView(R.layout.signin_layout)
+        signup_id = findViewById(R.id.signup_id)
+        signup_pw = findViewById(R.id.signup_pw)
+        sign_up = findViewById(R.id.signIn_signupBt)
+        login = findViewById(R.id.signIn_LoginBt)
 
-        val signup_id: EditText = findViewById(R.id.signup_id)
-        val signup_pw: EditText = findViewById(R.id.signup_pw)
-        val signin: Button = findViewById(R.id.signin)
-        val login: Button = findViewById(R.id.login)
 
-        login.setOnClickListener(){
-            if(signup_id.text.toString() == null || signup_pw.text.toString() == null){
+
+        login?.setOnClickListener(){
+            if(signup_id?.text.toString().isEmpty() || signup_pw?.text.toString().isEmpty()){
                 //사용자에게 실패메시지를 보여준다
                 Toast.makeText(this, "아이디나 비밀번호를 입력해주세요.", Toast.LENGTH_SHORT).show()
             }
             else{
                 //다음 화면으로 넘어가기
-                val intent = Intent(this@MainActivity, SignUpActivity::class.java)
-                intent.putExtra("login_id", signup_id.text.toString())
-                intent.putExtra("login_pw", signup_pw.text.toString())
-                startActivity(intent)
+
             }
         }
 
-        signin.setOnClickListener(){
+        sign_up?.setOnClickListener(){
 
+            val intent = Intent(this@MainActivity, SignUpActivity::class.java)
+            intent.putExtra("login_id", signup_id?.text.toString())
+            intent.putExtra("login_pw", signup_pw?.text.toString())
+            startActivityForResult(intent,3000)
+        }
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(resultCode == Activity.RESULT_OK){
+            when (requestCode) {
+                3000 -> {
+                    if (data!!.hasExtra("signup_id") && data!!.hasExtra("signup_pw")){
+                        val userid = data!!.getStringExtra("signup_id")
+                        val userpw = data!!.getStringExtra("signup_pw")
+
+                        signup_id?.setText(userid)
+                        signup_pw?.setText(userpw)
+                    }
+                    else{
+                        Toast.makeText(this, "aa", Toast.LENGTH_SHORT).show()
+
+                    }
+
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/example/sopt_homework1/SignUpActivity.kt
+++ app/src/main/java/com/example/sopt_homework1/SignUpActivity.kt
@@ -1,22 +1,147 @@
 package com.example.sopt_homework1
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-
-import kotlinx.android.synthetic.main.activity_sign_up.*
+import android.provider.MediaStore
+import java.io.IOException
+import java.io.File
+import java.io.FileOutputStream
+import android.os.Environment
+import android.graphics.Bitmap
+import java.io.ByteArrayOutputStream
+import android.media.MediaScannerConnection
+import java.util.*
 
 class SignUpActivity : AppCompatActivity() {
+    var signup_id: EditText? = null
+    var signup_pw: EditText? = null
+    var signup_name: EditText? = null
+    var signup_pwch: EditText? = null
+    var signup: Button? = null
+    var imageView: ImageView? = null
+    var camera_button: Button? = null
+    var gallery_button: Button? = null
+    private val GALLERY = 1
+    private val CAMERA = 2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
-        setSupportActionBar(toolbar)
+        signup_id = findViewById(R.id.id)
+        signup_pw = findViewById(R.id.pw)
+        signup_name = findViewById(R.id.name)
+        signup_pwch = findViewById(R.id.pw_check)
+        signup = findViewById(R.id.signIn_signupBt)
+        imageView = findViewById(R.id.imageview)
+        camera_button = findViewById(R.id.camera_button)
+        gallery_button = findViewById(R.id.gallery_button)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+
+        camera_button?.setOnClickListener(){
+            takePhotoFromCamera()
+        }
+
+        gallery_button?.setOnClickListener(){
+            choosePhotoFromGallary()
+        }
+
+        signup?.setOnClickListener(){
+            if(signup_id?.text.toString().isEmpty() || signup_pw?.text.toString().isEmpty() || signup_name?.text.toString().isEmpty() ||
+                signup_pwch?.text.toString().isEmpty()){
+                // 빈칸이 있으면 실패 메세지를 보여준다
+                Toast.makeText(this, "빈 칸 없이 입력해주세요", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                if(signup_pw?.text.toString().equals(signup_pwch?.text.toString())){
+                    //Toast.makeText(this, "회원가입 성공", Toast.LENGTH_SHORT).show()
+                    val intent = Intent()
+                    intent.putExtra("signup_id", signup_id?.text.toString())
+                    intent.putExtra("signup_pw", signup_pw?.text.toString())
+                    setResult(Activity.RESULT_OK, intent)
+                    finish()
+                }else{
+                    Toast.makeText(this, "비밀번호를 확인해주세요", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == GALLERY){
+            if (data != null)
+            {
+                val contentURI = data!!.data
+                try
+                {
+                    val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, contentURI)
+                    val path = saveImage(bitmap)
+                    imageView!!.setImageBitmap(bitmap)
+
+                }
+                catch (e: IOException) {
+                    e.printStackTrace()
+                    Toast.makeText(this, "Failed!", Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
+
+    fun choosePhotoFromGallary() {
+        val galleryIntent = Intent(Intent.ACTION_PICK,
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+
+        startActivityForResult(galleryIntent, GALLERY)
+    }
+
+    private fun takePhotoFromCamera() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(intent, CAMERA)
+    }
+
+    fun saveImage(myBitmap: Bitmap):String {
+        val bytes = ByteArrayOutputStream()
+        myBitmap.compress(Bitmap.CompressFormat.JPEG, 90, bytes)
+        val wallpaperDirectory = File(
+            (Environment.getExternalStorageDirectory()).toString() + IMAGE_DIRECTORY)
+        // have the object build the directory structure, if needed.
+        if (!wallpaperDirectory.exists())
+        {
+
+            wallpaperDirectory.mkdirs()
+        }
+
+        try
+        {
+            val f = File(wallpaperDirectory, ((Calendar.getInstance()
+                .getTimeInMillis()).toString() + ".jpg"))
+            f.createNewFile()
+            val fo = FileOutputStream(f)
+            fo.write(bytes.toByteArray())
+            MediaScannerConnection.scanFile(this,
+                arrayOf(f.getPath()),
+                arrayOf("image/jpeg"), null)
+            fo.close()
+
+            return f.getAbsolutePath()
+        }
+        catch (e1: IOException) {
+            e1.printStackTrace()
+        }
+
+        return ""
+    }
+
+    companion object {
+        private val IMAGE_DIRECTORY = "/demonuts"
+    }
+
 
 }
